7eea9a04632c6640cf2807d194441f6e304fda29
Subhajit Sarkar <sarkarsubhajit9499@gmail.com>
2023-07-16 21:48:00 +0530

    responsive design for index page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://Nick9499.github.io",
-  base: "/climate-care",
-  integrations: [tailwind()],
+  integrations: [tailwind()]
 });
